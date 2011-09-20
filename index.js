01c6e0ffc7bbce66c8ed65f5aa2106476581f4d7
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-09-19 20:37:44 -0700

    use node-bench for benchmarks

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var bench = require('./lib/bench');
+var fs = require('fs'),
+    _ = require('underscore'),
+    langs = {},
+    files = fs.readdirSync(__dirname + '/lib/languages/');
 
-bench.run();
+_.each(files, function (file) {
+    langs[file.replace('.js', '')] = require(__dirname + '/lib/languages/' + file);
+});
+
+exports.compare = (function () {
+    var cases = {};
+
+    _.each(langs, function (lang, key) {
+        lang.compile();
+
+        // cases[key + ' compile'] = function () {
+        //     lang.compile();
+        // };
+
+        cases[key + ' render'] = function () {
+            lang.render();
+        };
+    });
+
+    return cases;
+})();
+
+require('bench').runMain();
