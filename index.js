afe933d8aac6f83a197d41f266d319e15e46da47
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-09-20 19:45:24 -0700

    add nopt and allow different bench options

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var fs = require('fs'),
+var nopt = require('nopt'),
+    fs = require('fs'),
     _ = require('underscore'),
     langs = {},
-    files = fs.readdirSync(__dirname + '/lib/languages/');
+    files = fs.readdirSync(__dirname + '/lib/languages/'),
+    options = {
+        type: String,
+        time: Number,
+        comparisons: Number
+    },
+    shortOpts = {
+        t: ['--type'],
+        m: ['--time'],
+        c: ['--comparisons']
+    },
+    parsed = nopt(options, shortOpts, process.argv, 2);
+
+exports.time = parsed.time || 1000;
+exports.compareCount = parsed.compareCount || 8;
 
 _.each(files, function (file) {
     langs[file.replace('.js', '')] = require(__dirname + '/lib/languages/' + file);
@@ -11,15 +26,25 @@ exports.compare = (function () {
     var cases = {};
 
     _.each(langs, function (lang, key) {
-        lang.compile();
-
-        // cases[key + ' compile'] = function () {
-        //     lang.compile();
-        // };
-
-        cases[key + ' render'] = function () {
-            lang.render();
-        };
+        switch (parsed.type) {
+        case 'compile':
+            cases[key + ' compile'] = function () {
+                lang.compile();
+            };
+            break;
+        case 'render':
+            lang.compile();
+            cases[key + ' render'] = function () {
+                lang.render();
+            };
+            break;
+        default:
+            cases[key + ' complete'] = function () {
+                lang.compile();
+                lang.render();
+            };
+            break;
+        }
     });
 
     return cases;
