748be9d0ff4fd37ad959ed12b50cfab73533b712
Paul Armstrong <paul@paularmstrongdesigns.com>
2011-09-25 08:12:29 -0700

    adding makefile, scripts, and package.json

---
+++ scripts/config-lint.js
@@ -0,0 +1,42 @@
+module.exports = {
+    root: __dirname + '/../',
+    pathIgnore: ['*node_modules*']
+};
+
+var options = {
+    adsafe: false,
+    bitwise: true,
+    browser: false,
+    cap: false,
+    confusion: false,
+    continue: true,
+    css: false,
+    debug: false,
+    devel: true,
+    eqeq: false,
+    es5: true,
+    evil: false,
+    forin: false,
+    fragment: false,
+    indent: 4,
+    maxerr: 300,
+    maxlen: 600,
+    newcap: true,
+    node: true,
+    nomen: true,
+    on: false,
+    passfail: false,
+    plusplus: false,
+    predef: [],
+    regexp: true,
+    rhino: false,
+    safe: false,
+    sloppy: true,
+    sub: false,
+    undef: false,
+    unparam: false,
+    vars: false,
+    white: false,
+    widget: false,
+    windows: false
+};
